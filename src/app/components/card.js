0114482cd6b2f923d496ea8611efbc13a1be5c04
Alice Lira <alice.souza0310@gmail.com>
2024-06-12 10:37:25 -0300

    adicionado icone de entrada e saida

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,10 +1,17 @@
+'use client'
+import { BiChevronUpCircle } from "react-icons/bi"
+import { BiChevronDownCircle } from "react-icons/bi";
+
 export default function Card() {
     return (
         <div className="w-full absolute top-0 left-0 z-10">
             <div className="w-full absolute top-16 z-20 px-3"> 
                 <div className="bg-white p-3 rounded-md flex justify-evenly items-center">
                     <div className="flex flex-col">
-                        <p className="text-sm uppercase text-gray-500 font-medium">Entradas</p>
+                        <div className="flex gap-1 items-center justify-center">
+                            <p className="text-sm uppercase text-gray-500 font-medium ">Entradas</p>
+                            <BiChevronUpCircle className="text-green-600"/>
+                        </div>
                         <div className="flex gap-2 pt-2">
                             <span className="font-bold text-lg">R$</span>
                             <span className="font-bold text-lg">1.500</span>
@@ -13,7 +20,11 @@ export default function Card() {
                     </div>
 
                     <div className="flex flex-col ">
-                        <p className="text-sm uppercase text-gray-500 font-medium">Saídas</p>
+                        <div className="flex items-center justify-center gap-1">
+                            <p className="text-sm uppercase text-gray-500 font-medium">Saídas</p>
+                            <BiChevronDownCircle className="text-red-600"/>
+                        </div>
+
                         <div className="flex gap-2 pt-2">
                             <span className="font-bold text-lg">R$</span>
                             <span className="font-bold text-lg">1.500</span>
